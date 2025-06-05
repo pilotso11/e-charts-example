172f2c97e39b51751de06bdc42aa7f6b8a9665e1
pilotso11 <seth@oshers.com>
2025-06-05 10:32:26 +0100

    working with e-charts

--- go.mod
+++ go.mod
@@ -4,12 +4,14 @@ go 1.23.0
 
 toolchain go1.24.3
 
-require github.com/gin-gonic/gin v1.10.1
+require (
+	github.com/gin-gonic/gin v1.10.1
+	github.com/go-echarts/go-echarts/v2 v2.5.4
+)
 
 require (
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
@@ -19,6 +21,7 @@ require (
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/kr/text v0.1.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/types"
 )
 
 // ChartRequest represents the form data sent to the /chart endpoint
@@ -23,6 +29,94 @@ type ChartResponse struct {
 	Status  string      `json:"status"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+}
+
+// generateChart creates a chart using go-echarts based on the request parameters
+func generateChart(req ChartRequest) (string, error) {
+	// Initialize random number generator
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+
+	// Create a new line chart
+	line := charts.NewLine()
+
+	// Set chart title and subtitle
+	title := req.Metric
+	subtitle := "From " + req.From + " To " + req.To
+
+	// Set global options
+	line.SetGlobalOptions(
+		charts.WithInitializationOpts(opts.Initialization{
+			Theme: types.ThemeWesteros,
+		}),
+		charts.WithTitleOpts(opts.Title{
+			Title:    title,
+			Subtitle: subtitle,
+		}),
+	)
+
+	// Enable tooltip
+	line.SetGlobalOptions(charts.WithTooltipOpts(opts.Tooltip{
+		Trigger: "axis",
+	}))
+
+	// Enable legend
+	line.SetGlobalOptions(charts.WithLegendOpts(opts.Legend{}))
+
+	// Generate mock data
+	days := 30 // Mock data for 30 days
+	xAxis := make([]string, 0)
+
+	// Generate x-axis labels (dates)
+	startDate, _ := time.Parse("2006-01-02", req.From)
+	for i := 0; i < days; i++ {
+		date := startDate.AddDate(0, 0, i)
+		if date.After(time.Now()) {
+			break
+		}
+		xAxis = append(xAxis, date.Format("2006-01-02"))
+	}
+
+	// Set x-axis
+	line.SetXAxis(xAxis)
+
+	// Generate series data for each group
+	for _, group := range req.GroupBy {
+		// Generate random data for this group
+		data := make([]opts.LineData, 0)
+		for i := 0; i < len(xAxis); i++ {
+			// Generate random value between 10 and 100
+			value := random.Intn(90) + 10
+			data = append(data, opts.LineData{Value: value})
+		}
+
+		// Add the series to the chart
+		line.AddSeries(group, data)
+	}
+
+	// If no groups were selected, add a default series
+	if len(req.GroupBy) == 0 {
+		data := make([]opts.LineData, 0)
+		for i := 0; i < len(xAxis); i++ {
+			value := random.Intn(90) + 10
+			data = append(data, opts.LineData{Value: value})
+		}
+		line.AddSeries("Default", data)
+	}
+
+	// Set line style options
+	line.SetSeriesOptions(
+		charts.WithLineChartOpts(opts.LineChart{}),
+		charts.WithLabelOpts(opts.Label{}),
+	)
+
+	// Render the chart to HTML
+	buffer := bytes.Buffer{}
+	err := line.Render(&buffer)
+	if err != nil {
+		return "", err
+	}
+	return buffer.String(), nil
 }
 
 func main() {
@@ -49,8 +143,20 @@ func main() {
 			return
 		}
 
-		// Return the submitted JSON directly
-		c.JSON(http.StatusOK, chartReq)
+		// Generate a chart using go-echarts
+		html, err := generateChart(chartReq)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, ChartResponse{
+				Status:  "error",
+				Message: "Failed to generate chart: " + err.Error(),
+				Data:    nil,
+			})
+			return
+		}
+
+		// Return the HTML content
+		c.Header("Content-Type", "text/html")
+		c.String(http.StatusOK, html)
 	})
 
 	log.Println("Server starting on http://localhost:8080")
